a07a5b5a70be27d417d2ce538a6b77bf448a1dda
Sagnik Sasmal <16162562+SagnikSRHUSE@users.noreply.github.com>
2020-08-21 08:16:39 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,10 +3,16 @@ cd /home/container
 
 # Attempt Server Update
 mkdir /home/container/tmp/
+echo "Downloading latest server release"
 wget --no-check-certificate https://brokeprotocol.com/wp-content/uploads/game.tar.gz -O /home/container/tmp/game.tar.gz
+echo "Download complete"
+echo "Extracting files and overwriting existing files"
 tar -zxvf /home/container/tmp/game.tar.gz -C /home/container/tmp/
-rsync -ah --exclude "settings.json" --exclude "Maps" --exclude "game.tar.gz" /home/container/tmp/ /home/container/
+rsync -avh --exclude "settings.json" --exclude "Maps" --exclude "game.tar.gz" /home/container/tmp/ /home/container/
+echo "Update completed"
+echo "Cleaning up temporary files"
 rm -rf /home/container/tmp/
+echo "Resuming boot sequence"
 
 # Replace Startup Variables
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
