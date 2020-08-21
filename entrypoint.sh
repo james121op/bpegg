d2efc322fa61700d9092c04591be5951df09c4dc
Sagnik Sasmal <16162562+SagnikSRHUSE@users.noreply.github.com>
2020-08-21 08:13:11 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 cd /home/container
 
+# Attempt Server Update
+mkdir /home/container/tmp/
+wget --no-check-certificate https://brokeprotocol.com/wp-content/uploads/game.tar.gz -O /home/container/tmp/game.tar.gz
+tar -zxvf /home/container/tmp/game.tar.gz -C /home/container/tmp/
+rsync -ah --exclude "settings.json" --exclude "Maps" --exclude "game.tar.gz" /home/container/tmp/ /home/container/
+rm -rf /home/container/tmp/
+
 # Replace Startup Variables
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
 echo "${MODIFIED_STARTUP}"
