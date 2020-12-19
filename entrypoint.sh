7afee245b83ad6769feee0ae6d6448f28ba1e1c6
Sagnik Sasmal <16162562+SagnikSRHUSE@users.noreply.github.com>
2020-12-19 05:49:01 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,16 +2,16 @@
 cd /home/container
 
 # Attempt Server Update
-mkdir /home/container/tmp/
-cd /home/container/tmp/
-echo "Downloading and extracting latest server release"
-curl -L https://brokeprotocol.com/wp-content/uploads/game.tar.gz | tar -xz
-rsync -ah --exclude "settings.json" --exclude "Maps" --exclude "game.tar.gz" /home/container/tmp/ /home/container/
-echo "Update completed"
-cd /home/container/
-echo "Cleaning up temporary files"
-rm -rf /home/container/tmp/
-echo "Resuming boot sequence"
+#mkdir /home/container/tmp/
+#cd /home/container/tmp/
+#echo "Downloading and extracting latest server release"
+#curl -L https://brokeprotocol.com/wp-content/uploads/game.tar.gz | tar -xz
+#rsync -ah --exclude "settings.json" --exclude "Maps" --exclude "game.tar.gz" --exclude "groups.json" --exclude="whitelist.txt" --exclude="skins.txt" /home/container/tmp/ /home/container/
+#echo "Update completed"
+#cd /home/container/
+#echo "Cleaning up temporary files"
+#rm -rf /home/container/tmp/
+#echo "Resuming boot sequence"
 
 # Replace Startup Variables
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
