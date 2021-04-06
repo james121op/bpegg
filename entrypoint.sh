355be6ea453d36b44029fe103cf8efb6252c3297
james121op <james121op@gmail.com>
2021-04-06 13:11:38 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,21 +1,69 @@
 #!/bin/bash
 cd /home/container
 
-# Attempt Server Update
-#mkdir /home/container/tmp/
-#cd /home/container/tmp/
-#echo "Downloading and extracting latest server release"
-#curl -L https://brokeprotocol.com/wp-content/uploads/game.tar.gz | tar -xz
-#rsync -ah --exclude "settings.json" --exclude "Maps" --exclude "game.tar.gz" --exclude "groups.json" --exclude="whitelist.txt" --exclude="skins.txt" /home/container/tmp/ /home/container/
-#echo "Update completed"
-#cd /home/container/
-#echo "Cleaning up temporary files"
-#rm -rf /home/container/tmp/
-#echo "Resuming boot sequence"
+#Server Update
+function StartUp() {
+    echo "StartUp()"
+    mkdir /home/container/tmp/
+    cd /home/container/tmp/
+    mkdir filesafe
+    curl https://brokeprotocol.com/version -o serverversion
+    serverversion=$(cat serverversion)
+    if [ -f LocalVersion ]; then
+        Update
+    else
+        FirstTimeSetup
+    fi
+}
+function Update() {
+    echo "Update()"
+    localversion=$(cat localversion)
+    if [ $localversion == $serverversion ]; then
+        echo "your already updated!"
+        Done
+    fi
+    curl https://brokeprotocol.com/wp-content/uploads/game.tar.gz -o bp.tar.gz
+    # backup files
+    cp Maps filesafe
+    cp www filesafe
+    cp Plugins filesafe
+    cp server_info.txt filesafe
+    cp announcements.txt filesafe
+    cp groups.json filesafe
+    cp settings.json filesafe
+    cp skins.txt filesafe
+    cp whitelist.txt filesafe
+    cp npc_names.txt filesafe
+    tar xvzf bp.tar.gz
+    rm -rf bp.tar.gz
+    #put files back (not plugins they need to be updated)
+    cp filesafe/Maps .
+    cp filesafe/www .
+    cp filesafe/server_info.txt .
+    cp filesafe/announcements.txt .
+    cp filesafe/groups.json .
+    cp filesafe/settings.json .
+    cp filesafe/skins.txt .
+    cp filesafe/whitelist.txt .
+    cp filesafe/npc_names.txt .
+    echo "done"
+    echo "you might need to update settings.json"
+    sleep 5
+    Done
+}
+function FirstTimeSetup() {
+    echo "FirstTimeSetup()"
+    curl https://brokeprotocol.com/version -o localversion
+    curl https://brokeprotocol.com/wp-content/uploads/game.tar.gz -o bp.tar.gz
+    tar xvzf bp.tar.gz
+}
+StartUp
+function Done() {
+    chmod +x *
+    # Replace Startup Variables
+    MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
+    echo "${MODIFIED_STARTUP}"
 
-# Replace Startup Variables
-MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
-echo "${MODIFIED_STARTUP}"
-
-# Run the Server
-eval ${MODIFIED_STARTUP}
+    # Run the Server  
+    eval ${MODIFIED_STARTUP}
+}
