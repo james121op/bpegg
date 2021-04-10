82092c47d48b6e1db4f5bbe37fba8e98828c82e3
james121op <james121op@gmail.com>
2021-04-10 11:15:36 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -67,6 +67,10 @@ function FirstTimeSetup() {
     curl https://brokeprotocol.com/wp-content/uploads/game.tar.gz -sSLo bp.tar.gz
     tar xzf bp.tar.gz
     rm -rf bp.tar.gz
+    rm -rf settings.json
+    # jq doesn't support jsonc
+    echo '{"serverName":"Unconfigured Server","map":"Default","players":32,"hostName":"","port":5557,"maxTransferRate":1000000,"whitelist":false,"dayLength":1440,"difficulty":0.5,"startItems":[{"itemName":"Money","chance":1,"minCount":1000,"maxCount":1000},{"itemName":"SmartPhone1","chance":1,"minCount":1,"maxCount":1}],"cef":false,"announcements":{"enabled":true,"interval":120},"http":{"port":8080,"https":false,"internalHost":"*"},"database":{"path":null,"connectionString":null}}' > settings.json
+    echo "$( jq ".port = "${SERVER_PORT}"" settings.json )" > settings.json
     Done
     exit 0
 }
