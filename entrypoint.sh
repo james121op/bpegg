0338b8176a197202efe66454dbddb8dbeff9cd4a
James <me@james121op.me>
2022-12-01 20:13:51 +0000

    revert (kind of)

--- entrypoint.sh
+++ entrypoint.sh
@@ -49,6 +49,10 @@ function FirstTimeSetup() {
 function Done() {
     # Replace Startup Variables
     MODIFIED_STARTUP=$(eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g'))
+    #for people still using the old version of the egg
+    if [ -f server.log ]; then
+        ln -sf /dev/stdout server.log
+    fi
     #start bp server
     eval ${MODIFIED_STARTUP}
 }
